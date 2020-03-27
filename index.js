541147aa59038050efcc5cacec7af0b900bfb359
prapawit201 <piyapat43@gmail.com>
2020-03-27 19:09:59 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const keys = require("./config/key");
+
+// const connectDB = require("./Database/Connection");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+// connectDB();
+//?or
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+console.log("Server run port " + PORT);
