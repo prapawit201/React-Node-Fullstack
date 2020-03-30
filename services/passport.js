8ec1241bb8c43a1a2bfe2d1344f02cdb18a56dad
prapawit201 <piyapat43@gmail.com>
2020-03-30 22:25:06 +0700

    [Add] google proxy

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
-      proxy: true
+      proxy: true //add proxy
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
