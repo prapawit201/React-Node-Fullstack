ad1fdfda4988a18213672649550e7791f900794c
prapawit201 <piyapat43@gmail.com>
2020-03-30 22:22:16 +0700

    google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "https://badlands.herokuapp.com/auth/google/callback",
+      callbackURL: "/auth/google/callback",
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
