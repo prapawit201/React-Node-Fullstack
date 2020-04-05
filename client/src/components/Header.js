09a25d6afd427108587a72619c64dfc3634a2833
prapawit201 <piyapat43@gmail.com>
2020-04-05 16:46:22 +0700

    [Add] materialize css Header page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,20 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 class Header extends Component {
-    render(){
-        return (
-            <div>
-                Header
-            </div>
-        )
-    }
+  render() {
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a class="brand-logo">zBabEs</a>
+          <ul class="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
+
+export default Header;
