356b60cd6032829cb445088093ddee3a3a2f28b0
prapawit201 <piyapat43@gmail.com>
2020-04-07 01:13:14 +0700

    [Add] axios

--- client/README.md
+++ client/README.md
@@ -5,3 +5,7 @@
 ###### npm install materialize-css@next
 
 ###### https://materializecss.com/getting-started.html
+
+##### --2 Add proxy connection
+
+###### npm install --save axios redux-thunk
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,4 +3,6 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(proxy(["/api", "/auth/google"], { target: "http://localhost:5000" }));
   app.use(proxy("/api/**", { target: "http://localhost:5000" }));
+  app.use(proxy("/api/current_user", { target: "http://localhost:5000" }));
+  
 };
